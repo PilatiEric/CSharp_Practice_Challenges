a0d07cb1c1063c00a2628fe674b41cc64e74d0ef
PilatiEric <79669689+PilatiEric@users.noreply.github.com>
2023-07-13 12:26:04 -0400

    Update Challenges (Levels 19-25).cs
    Minor edits

--- Challenges (Levels 19-25).cs
+++ Challenges (Levels 19-25).cs
@@ -5,318 +5,317 @@
 //C# Players Guide - Challenge "Vin's Trouble" pg. 162
 
 
-//Arrow product = new Arrow(HeadType.Steel, 1, FletchingType.Turkey);
+Arrow product = new Arrow(HeadType.Steel, 1, FletchingType.Turkey);
 
-//void get_product()
-//{
-//    bool head_selected = false;
-//    bool shaft_selected = false;
-//    bool fletch_selected = false;
+void get_product()
+{
+    bool head_selected = false;
+    bool shaft_selected = false;
+    bool fletch_selected = false;
 
-//    //Get arrowhead type
-//    while (head_selected == false)
-//    {
-//        Console.WriteLine("What material would you like the arrowhead to be made of?");
-//        Console.WriteLine("1) Steel");
-//        Console.WriteLine("2) Wood");
-//        Console.WriteLine("3) Obsidian");
-//        Console.Write("Please enter the corresponding number: ");
-//        int head_num = Convert.ToInt32(Console.ReadLine());
+    //Get arrowhead type
+    while (head_selected == false)
+    {
+        Console.WriteLine("What material would you like the arrowhead to be made of?");
+        Console.WriteLine("1) Steel");
+        Console.WriteLine("2) Wood");
+        Console.WriteLine("3) Obsidian");
+        Console.Write("Please enter the corresponding number: ");
+        int head_num = Convert.ToInt32(Console.ReadLine());
 
-//        switch (head_num)
-//        {
-//            case 1:
-//                product.SetHeadType(HeadType.Steel);
-//                head_selected = true;
-//                break;
-//            case 2:
-//                product.SetHeadType(HeadType.Wood);
-//                head_selected = true;
-//                break;
-//            case 3:
-//                product.SetHeadType(HeadType.Obsidian);
-//                head_selected = true;
-//                break;
-//            default:
-//                Console.WriteLine("That is an invalid entry. Please select a correct one.");
-//                break;
-//        }
-//    }
+        switch (head_num)
+        {
+            case 1:
+                product.SetHeadType(HeadType.Steel);
+                head_selected = true;
+                break;
+            case 2:
+                product.SetHeadType(HeadType.Wood);
+                head_selected = true;
+                break;
+            case 3:
+                product.SetHeadType(HeadType.Obsidian);
+                head_selected = true;
+                break;
+            default:
+                Console.WriteLine("That is an invalid entry. Please select a correct one.");
+                break;
+        }
+    }
 
-//    //Get shaft length
-//    while (shaft_selected == false)
-//    {
-//        Console.Write("Please enter how many centimeters long the shaft should be: ");
-//        double shaft_num = Convert.ToInt32(Console.ReadLine());
+    //Get shaft length
+    while (shaft_selected == false)
+    {
+        Console.Write("Please enter how many centimeters long the shaft should be: ");
+        double shaft_num = Convert.ToInt32(Console.ReadLine());
 
-//        if (shaft_num > 0)
-//        {
-//            product.SetShaftLength(shaft_num);
-//            shaft_selected = true;
-//        }
-//        else
-//            Console.WriteLine("The number must be greater than 0.");
-//    }
+        if (shaft_num > 0)
+        {
+            product.SetShaftLength(shaft_num);
+            shaft_selected = true;
+        }
+        else
+            Console.WriteLine("The number must be greater than 0.");
+    }
 
-//    //Get fletching type
-//    while (fletch_selected == false)
-//    {
-//        Console.WriteLine("What material would you like the fletching to be made of?");
-//        Console.WriteLine("1) Plastic");
-//        Console.WriteLine("2) Turkey Feathers");
-//        Console.WriteLine("3) Goose Feathers");
-//        Console.Write("Please enter the corresponding number: ");
-//        int fletch_num = Convert.ToInt32(Console.ReadLine());
+    //Get fletching type
+    while (fletch_selected == false)
+    {
+        Console.WriteLine("What material would you like the fletching to be made of?");
+        Console.WriteLine("1) Plastic");
+        Console.WriteLine("2) Turkey Feathers");
+        Console.WriteLine("3) Goose Feathers");
+        Console.Write("Please enter the corresponding number: ");
+        int fletch_num = Convert.ToInt32(Console.ReadLine());
 
-//        switch (fletch_num)
-//        {
-//            case 1:
-//                product.SetFletchingType(FletchingType.Plastic);
-//                fletch_selected = true;
-//                break;
-//            case 2:
-//                product.SetFletchingType(FletchingType.Turkey);
-//                fletch_selected = true;
-//                break;
-//            case 3:
-//                product.SetFletchingType(FletchingType.Goose);
-//                fletch_selected = true;
-//                break;
-//            default:
-//                Console.WriteLine("That is an invalid entry. Please select a correct one.");
-//                break;
-//        }
-//    }
-//}
-
-
-////Determine cost of arrow
-//double get_cost()
-//{
-//    double arrow_cost = 0.0;
-
-//    //Add arrowhead material cost
-//    if (product.GetHeadType() == HeadType.Steel)
-//        arrow_cost += 10;
-//    else if (product.GetHeadType() == HeadType.Wood)
-//        arrow_cost += 3;
-//    else
-//        arrow_cost += 5;
-
-//    //Add shaft length cost
-//    arrow_cost += product.GetShaftLength() * 0.5;
-
-//    //Add fletching material cost
-//    if (product.GetFletchMaterial() == FletchingType.Plastic)
-//        arrow_cost += 10;
-//    else if (product.GetFletchMaterial() == FletchingType.Turkey)
-//        arrow_cost += 5;
-//    else
-//        arrow_cost += 3;
-
-//    return arrow_cost;
-//}
-
-////Display total cost
-//get_product();
-//Console.WriteLine($"The total cost for an arrow with an " +
-//    $"arrowhead made of {product.GetHeadType()}, " +
-//    $"a shaft that's {product.GetShaftLength()}cm long, " +
-//    $"and fletching made of {product.GetFletchMaterial()} " +
-//    $"is {get_cost()} gold.");
+        switch (fletch_num)
+        {
+            case 1:
+                product.SetFletchingType(FletchingType.Plastic);
+                fletch_selected = true;
+                break;
+            case 2:
+                product.SetFletchingType(FletchingType.Turkey);
+                fletch_selected = true;
+                break;
+            case 3:
+                product.SetFletchingType(FletchingType.Goose);
+                fletch_selected = true;
+                break;
+            default:
+                Console.WriteLine("That is an invalid entry. Please select a correct one.");
+                break;
+        }
+    }
+}
 
 
+//Determine cost of arrow
+double get_cost()
+{
+    double arrow_cost = 0.0;
 
-////Classes and Enumerations for Vin Fletcher's Arrows
-////Make fields private and add getters and setters
-//class Arrow
-//{
-//    private HeadType head_material;
-//    private double shaft_length;
-//    private FletchingType fletch_material;
+    //Add arrowhead material cost
+    if (product.GetHeadType() == HeadType.Steel)
+        arrow_cost += 10;
+    else if (product.GetHeadType() == HeadType.Wood)
+        arrow_cost += 3;
+    else
+        arrow_cost += 5;
 
-//    public Arrow (HeadType head_material, double shaft_length, FletchingType fletch_material)
-//    {
-//        this.head_material = head_material;
-//        this.shaft_length = shaft_length;
-//        this.fletch_material = fletch_material;
-//    }
+    //Add shaft length cost
+    arrow_cost += product.GetShaftLength() * 0.5;
 
-//    public HeadType GetHeadType() => this.head_material;
-//    public double GetShaftLength() => this.shaft_length;
-//    public FletchingType GetFletchMaterial() => this.fletch_material;
+    //Add fletching material cost
+    if (product.GetFletchMaterial() == FletchingType.Plastic)
+        arrow_cost += 10;
+    else if (product.GetFletchMaterial() == FletchingType.Turkey)
+        arrow_cost += 5;
+    else
+        arrow_cost += 3;
 
-//    public void SetHeadType(HeadType value) => this.head_material = value;
-//    public void SetShaftLength(double value) => this.shaft_length = value;
-//    public void SetFletchingType(FletchingType value) => this.fletch_material = value;
-//}
+    return arrow_cost;
+}
+//Display total cost
+get_product();
+Console.WriteLine($"The total cost for an arrow with an " +
+    $"arrowhead made of {product.GetHeadType()}, " +
+    $"a shaft that's {product.GetShaftLength()}cm long, " +
+    $"and fletching made of {product.GetFletchMaterial()} " +
+    $"is {get_cost()} gold.");
 
-//enum HeadType
-//{
-//    Steel,
-//    Wood,
-//    Obsidian
-//}
 
-//enum FletchingType
-//{
-//    Plastic,
-//    Turkey,
-//    Goose
-//}
 
+//Classes and Enumerations for Vin Fletcher's Arrows
+//Make fields private and add getters and setters
+class Arrow
+{
+    private HeadType head_material;
+    private double shaft_length;
+    private FletchingType fletch_material;
+
+    public Arrow (HeadType head_material, double shaft_length, FletchingType fletch_material)
+    {
+        this.head_material = head_material;
+        this.shaft_length = shaft_length;
+        this.fletch_material = fletch_material;
+    }
+
+    public HeadType GetHeadType() => this.head_material;
+    public double GetShaftLength() => this.shaft_length;
+    public FletchingType GetFletchMaterial() => this.fletch_material;
+
+    public void SetHeadType(HeadType value) => this.head_material = value;
+    public void SetShaftLength(double value) => this.shaft_length = value;
+    public void SetFletchingType(FletchingType value) => this.fletch_material = value;
+}
+
+enum HeadType
+{
+    Steel,
+    Wood,
+    Obsidian
+}
+
+enum FletchingType
+{
+    Plastic,
+    Turkey,
+    Goose
+}
 
 
 
 
 
 
-////C# Players Guide - Challenge "The Properties of Arrows" Level 20, pg. 168
 
-//Arrow product = new Arrow();
+//C# Players Guide - Challenge "The Properties of Arrows" Level 20, pg. 168
 
-//void get_product()
-//{
-//    bool head_selected = false;
-//    bool shaft_selected = false;
-//    bool fletch_selected = false;
+Arrow product = new Arrow();
 
-//    //Get arrowhead type
-//    while (head_selected == false)
-//    {
-//        Console.WriteLine("What material would you like the arrowhead to be made of?");
-//        Console.WriteLine("1) Steel");
-//        Console.WriteLine("2) Wood");
-//        Console.WriteLine("3) Obsidian");
-//        Console.Write("Please enter the corresponding number: ");
-//        int head_num = Convert.ToInt32(Console.ReadLine());
+void get_product()
+{
+    bool head_selected = false;
+    bool shaft_selected = false;
+    bool fletch_selected = false;
 
-//        switch (head_num)
-//        {
-//            case 1:
-//                product.headtype = HeadType.Steel;
-//                head_selected = true;
-//                break;
-//            case 2:
-//                product.headtype = HeadType.Wood;
-//                head_selected = true;
-//                break;
-//            case 3:
-//                product.headtype = HeadType.Obsidian;
-//                head_selected = true;
-//                break;
-//            default:
-//                Console.WriteLine("That is an invalid entry. Please select a correct one.");
-//                break;
-//        }
-//    }
+    //Get arrowhead type
+    while (head_selected == false)
+    {
+        Console.WriteLine("What material would you like the arrowhead to be made of?");
+        Console.WriteLine("1) Steel");
+        Console.WriteLine("2) Wood");
+        Console.WriteLine("3) Obsidian");
+        Console.Write("Please enter the corresponding number: ");
+        int head_num = Convert.ToInt32(Console.ReadLine());
 
-//    //Get shaft length
-//    while (shaft_selected == false)
-//    {
-//        Console.Write("Please enter how many centimeters long the shaft should be: ");
-//        double shaft_num = Convert.ToInt32(Console.ReadLine());
+        switch (head_num)
+        {
+            case 1:
+                product.headtype = HeadType.Steel;
+                head_selected = true;
+                break;
+            case 2:
+                product.headtype = HeadType.Wood;
+                head_selected = true;
+                break;
+            case 3:
+                product.headtype = HeadType.Obsidian;
+                head_selected = true;
+                break;
+            default:
+                Console.WriteLine("That is an invalid entry. Please select a correct one.");
+                break;
+        }
+    }
 
-//        if (shaft_num > 0)
-//        {
-//            product.shaftlength = shaft_num;
-//            shaft_selected = true;
-//        }
-//        else
-//            Console.WriteLine("The number must be greater than 0.");
-//    }
+    //Get shaft length
+    while (shaft_selected == false)
+    {
+        Console.Write("Please enter how many centimeters long the shaft should be: ");
+        double shaft_num = Convert.ToInt32(Console.ReadLine());
 
-//    //Get fletching type
-//    while (fletch_selected == false)
-//    {
-//        Console.WriteLine("What material would you like the fletching to be made of?");
-//        Console.WriteLine("1) Plastic");
-//        Console.WriteLine("2) Turkey Feathers");
-//        Console.WriteLine("3) Goose Feathers");
-//        Console.Write("Please enter the corresponding number: ");
-//        int fletch_num = Convert.ToInt32(Console.ReadLine());
+        if (shaft_num > 0)
+        {
+            product.shaftlength = shaft_num;
+            shaft_selected = true;
+        }
+        else
+            Console.WriteLine("The number must be greater than 0.");
+    }
 
-//        switch (fletch_num)
-//        {
-//            case 1:
-//                product.fletchtype = FletchingType.Plastic;
-//                fletch_selected = true;
-//                break;
-//            case 2:
-//                product.fletchtype = FletchingType.Turkey;
-//                fletch_selected = true;
-//                break;
-//            case 3:
-//                product.fletchtype = FletchingType.Goose;
-//                fletch_selected = true;
-//                break;
-//            default:
-//                Console.WriteLine("That is an invalid entry. Please select a correct one.");
-//                break;
-//        }
-//    }
-//}
+    //Get fletching type
+    while (fletch_selected == false)
+    {
+        Console.WriteLine("What material would you like the fletching to be made of?");
+        Console.WriteLine("1) Plastic");
+        Console.WriteLine("2) Turkey Feathers");
+        Console.WriteLine("3) Goose Feathers");
+        Console.Write("Please enter the corresponding number: ");
+        int fletch_num = Convert.ToInt32(Console.ReadLine());
 
-
-////Determine cost of arrow
-//double get_cost()
-//{
-//    double arrow_cost = 0.0;
-
-//    //Add arrowhead material cost
-//    if (product.headtype == HeadType.Steel)
-//        arrow_cost += 10;
-//    else if (product.headtype == HeadType.Wood)
-//        arrow_cost += 3;
-//    else
-//        arrow_cost += 5;
-
-//    //Add shaft length cost
-//    arrow_cost += product.shaftlength * 0.5;
-
-//    //Add fletching material cost
-//    if (product.fletchtype == FletchingType.Plastic)
-//        arrow_cost += 10;
-//    else if (product.fletchtype == FletchingType.Turkey)
-//        arrow_cost += 5;
-//    else
-//        arrow_cost += 3;
-
-//    return arrow_cost;
-//}
-
-////Display total cost
-//get_product();
-//Console.WriteLine($"The total cost for an arrow with an " +
-//    $"arrowhead made of {product.headtype}, " +
-//    $"a shaft that's {product.shaftlength}cm long, " +
-//    $"and fletching made of {product.fletchtype} " +
-//    $"is {get_cost()} gold.");
+        switch (fletch_num)
+        {
+            case 1:
+                product.fletchtype = FletchingType.Plastic;
+                fletch_selected = true;
+                break;
+            case 2:
+                product.fletchtype = FletchingType.Turkey;
+                fletch_selected = true;
+                break;
+            case 3:
+                product.fletchtype = FletchingType.Goose;
+                fletch_selected = true;
+                break;
+            default:
+                Console.WriteLine("That is an invalid entry. Please select a correct one.");
+                break;
+        }
+    }
+}
 
 
+//Determine cost of arrow
+double get_cost()
+{
+    double arrow_cost = 0.0;
 
-////Classes and Enumerations for Vin Fletcher's Arrows
-////Make fields private and add getters and setters
-//class Arrow
-//{
-//    public HeadType headtype { get; set; }
-//    public double shaftlength { get; set; }
-//    public FletchingType fletchtype { get; set; }
-//}
+    //Add arrowhead material cost
+    if (product.headtype == HeadType.Steel)
+        arrow_cost += 10;
+    else if (product.headtype == HeadType.Wood)
+        arrow_cost += 3;
+    else
+        arrow_cost += 5;
 
-//enum HeadType
-//{
-//    Steel,
-//    Wood,
-//    Obsidian
-//}
+    //Add shaft length cost
+    arrow_cost += product.shaftlength * 0.5;
 
-//enum FletchingType
-//{
-//    Plastic,
-//    Turkey,
-//    Goose
-//}
+    //Add fletching material cost
+    if (product.fletchtype == FletchingType.Plastic)
+        arrow_cost += 10;
+    else if (product.fletchtype == FletchingType.Turkey)
+        arrow_cost += 5;
+    else
+        arrow_cost += 3;
+
+    return arrow_cost;
+}
+
+//Display total cost
+get_product();
+Console.WriteLine($"The total cost for an arrow with an " +
+    $"arrowhead made of {product.headtype}, " +
+    $"a shaft that's {product.shaftlength}cm long, " +
+    $"and fletching made of {product.fletchtype} " +
+    $"is {get_cost()} gold.");
+
+
+
+//Classes and Enumerations for Vin Fletcher's Arrows
+//Make fields private and add getters and setters
+class Arrow
+{
+    public HeadType headtype { get; set; }
+    public double shaftlength { get; set; }
+    public FletchingType fletchtype { get; set; }
+}
+
+enum HeadType
+{
+    Steel,
+    Wood,
+    Obsidian
+}
+
+enum FletchingType
+{
+    Plastic,
+    Turkey,
+    Goose
+}
 
 
 
